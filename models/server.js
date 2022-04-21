f9a40a9f7c3c7f10a2f037bb030a115ae3b4476a
Felipe Paez <ofpaez87@gmail.com>
2022-04-21 17:21:20 -0500

    Ruta busqueda productos, usuarios, categorias

--- models/server.js
+++ models/server.js
@@ -12,6 +12,7 @@ class Server {
         // Rutas de categorias
         this.paths = {
             auth: '/api/auth',
+            buscar: '/api/buscar/',
             usuarios: '/api/usuarios',
             productos: '/api/productos',
             categorias: '/api/categorias',
@@ -44,6 +45,7 @@ class Server {
 
     routes(){
         this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
